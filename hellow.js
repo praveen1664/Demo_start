755788f1bde9f1bd0c355da97bbe8066b64aa413
praveen1664 <praveen1664@outlook.com>
2018-05-27 16:40:55 +0530

    My Demo Preparations

---
+++ hellow.js
@@ -0,0 +1,14 @@
+console.log('Hellow World');
+var v1 =1123
+for(let a= 1; a<100; a++){
+    console.log(a)
+}
+const cv=3.14
+
+function f1(){
+    console.log(v1)
+    var lo= "Local_scope"
+    console.log(lo)
+}
+
+console.log(lo)
---
+++ test.js
@@ -0,0 +1,10 @@
+console.log(a)
+var a=10
+console.log(a)
+console.log(funcdef)
+funcdef()
+var funcdef=function(){
+    console.log('some function')
+
+}
+funcdef()
